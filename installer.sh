3a55a9ab21e9939e0be3fa93b2663035e5dd8c20
Camden <53316692+humeman@users.noreply.github.com>
2020-02-03 18:41:29 -0600

    Fix installer.sh (2)
    Added: Better error handling for already existing directories & fixed syntax error

--- installer.sh
+++ installer.sh
@@ -14,8 +14,40 @@ error () {
 }
 
 # Logging function
-log() {
+log () {
     echo -e "\e[1m\e[33m[OAInstaller] \e[97m$1\e[0m\e[37m"
+}
+
+check () {
+    log "$1 [y/n]"
+    read answer
+    if [ $answer == "y" ]
+    then
+        return TRUE
+    elif [ answer == "n" ]
+    then
+        return FALSE
+    else
+        log "Please enter y/n."
+        check "$1"
+    fi
+}
+
+check_directory () {
+    if [ -d $1 ]
+    then
+        log "Directory ~/$2 already exists!"
+        check "Would you like to overwrite it and start fresh?"
+        if [ $? ]
+        then
+            sudo rm -rf $1
+            return TRUE
+        else
+            log "Keeping current $2 directory."
+            return FALSE
+    else
+        return TRUE
+    fi
 }
 
 # Update apt
@@ -23,7 +55,7 @@ log "Updating APT"
 sudo apt update
 error "Couldn't update apt!"
 
-# Install all 44dependencies
+# Install all dependencies
 log "Installing dependencies"
 sudo apt-get install -y libboost-all-dev libusb-1.0.0-dev libssl-dev cmake libprotobuf-dev protobuf-c-compiler protobuf-compiler libqt5multimedia5 libqt5multimedia5-plugins libqt5multimediawidgets5 qtmultimedia5-dev libqt5bluetooth5 libqt5bluetooth5-bin qtconnectivity5-dev pulseaudio librtaudio-dev libraspberrypi-doc libraspberrypi-dev
 error "Couldn't install all dependencies! Is Raspbian up to date?"
@@ -31,13 +63,21 @@ error "Couldn't install all dependencies! Is Raspbian up to date?"
 # Clone AASDK
 log "Cloning AASDK"
 cd ~
-git clone -b development https://github.com/abraha2d/aasdk aasdk
-error "Couldn't clone AASDK!"
+check_directory aasdk "aasdk"
+if [ $? ]
+then
+    git clone -b development https://github.com/abraha2d/aasdk aasdk
+    error "Couldn't clone AASDK!"
+fi
 
 # Build AASDK
 log "Building AASDK"
 cd ~
-mkdir aasdk_build
+check_directory aasdk_build "aasdk_build"
+if [ $? ]
+then
+    mkdir aasdk_build
+fi
 cd aasdk_build
 cmake -DCMAKE_BUILD_TYPE=Release ~/aasdk
 error "Couldn't run CMake on AASDK!"
@@ -53,15 +93,23 @@ error "Couldn't build ilclient!"
 # Clone OpenAuto
 log "Cloning OpenAuto"
 cd ~
-git clone -b development https://github.com/humeman/openauto openauto
-error "Couldn't clone OpenAuto!"
+check_directory openauto "openauto"
+if [ $? ]
+then
+    git clone -b development https://github.com/humeman/openauto openauto
+    error "Couldn't clone openauto!"
+fi
 
 # Build OpenAuto
 log "Building OpenAuto"
 cd ~
-mkdir openauto_build
+check_directory openauto_build "openauto_build"
+if [ $? ]
+then
+    mkdir openauto_build
+fi
 cd openauto_build
-cmake -DCMAKE_BUILD_TYPE=Release -DRPI3_BUILD=TRUE -DAASDK_INCLUDE_DIRS="/home/$USER/aasdk/include" -DAASDK_LIBRARIES="/home/$USER/aasdk/lib/libaasdk.so" -DAASDK_PROTO_INCLUDE_DIRS="/home/$USER/aasdk_build" -DAASDK_PROTO_LIBRARIES="/home/$USER/aasdk/lib/libaasdk_proto.so" .~/openauto
+cmake -DCMAKE_BUILD_TYPE=Release -DRPI3_BUILD=TRUE -DAASDK_INCLUDE_DIRS="/home/$USER/aasdk/include" -DAASDK_LIBRARIES="/home/$USER/aasdk/lib/libaasdk.so" -DAASDK_PROTO_INCLUDE_DIRS="/home/$USER/aasdk_build" -DAASDK_PROTO_LIBRARIES="/home/$USER/aasdk/lib/libaasdk_proto.so" ~/openauto
 error "Couldn't run CMake on OpenAuto!"
 make -j2
 error "Couldn't build OpenAuto!"
@@ -69,5 +117,5 @@ error "Couldn't build OpenAuto!"
 # Finish up
 log "Done!"
 log "To start OpenAuto in the future, run \e[1msudo ~/openauto/bin/autoapp"
-log "Starting for first time..."
+log "Starting for first time now. If nothing happens when you plug in your phone, please try restarting."
 sudo ~/openauto/bin/autoapp
